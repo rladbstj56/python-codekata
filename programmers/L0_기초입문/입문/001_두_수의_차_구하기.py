4440cfc888e8f9257c2efc948a0b7c7c37122531
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-01-19 11:18:39 +0900

    [프로그래머스] L0 - 두 수의 차 구하기 (#1)

--- programmers/L0_기초입문/입문/001_두_수의_차_구하기.py
+++ programmers/L0_기초입문/입문/001_두_수의_차_구하기.py
@@ -3,8 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 01. 19. 10:55:28
+# 작성일: 2026. 01. 19. 11:18:39
 
 def solution(num1, num2):
+    if num1>50000 or num1<-50000 or num2>50000 or num2<-50000:
+        return False
     answer = num1 - num2
     return answer
