c947d749c0a2373f4eff3db009a03fe14d686485
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-01-20 12:04:47 +0900

    [프로그래머스] L0 - 각도기 (#9)

--- programmers/L0_기초입문/입문/009_각도기.py
+++ programmers/L0_기초입문/입문/009_각도기.py
@@ -3,14 +3,16 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120829
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 01. 20. 12:02:12
+# 작성일: 2026. 01. 20. 12:04:46
 
 def solution(angle):
-    if angle > 0 and angle < 90:
+    if 0< angle < 90:
         return 1
     elif angle == 90: 
         return 2
     elif angle < 180: 
         return 3
-    else:
+    elif angle == 180:
         return 4
+    else:
+        raise ValueError("0 < x <= 180 범위 내로만 입력 가능합니다.")
