1307e2a34c73ffa86fbc1bc01c59aff8ab8d5492
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-01-20 11:20:39 +0900

    [프로그래머스] L0 - 나머지 구하기 (#5)

--- programmers/L0_기초입문/입문/005_나머지_구하기.py
+++ programmers/L0_기초입문/입문/005_나머지_구하기.py
@@ -3,10 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120810
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 01. 20. 11:06:29
+# 작성일: 2026. 01. 20. 11:20:38
 
 def solution(num1, num2):
-    try:
-        return num1%num2
-    except ZeroDivisionError:
-        print("0으로 나눌 수 없습니다.")
+    return num1%num2
