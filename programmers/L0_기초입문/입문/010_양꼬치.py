4942fa2c34a24fbdc5afb9c87f3e815fbecae748
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-01-21 10:04:01 +0900

    [프로그래머스] L0 - 양꼬치 (#10)

--- programmers/L0_기초입문/입문/010_양꼬치.py
+++ programmers/L0_기초입문/입문/010_양꼬치.py
@@ -3,9 +3,8 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120830
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 01. 21. 10:00:05
+# 작성일: 2026. 01. 21. 10:04:00
 
 def solution(n, k):
-    service = n//10
-    price = 12000*n + 2000*(k-service)
-    return price
+    k -= n//10
+    return 12000*n + 2000*k
