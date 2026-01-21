24352e0a33a9db5222f60f31258f880528929aa3
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-01-21 10:19:09 +0900

    [프로그래머스] L0 - 짝수의 합 (#11)

--- programmers/L0_기초입문/입문/011_짝수의_합.py
+++ programmers/L0_기초입문/입문/011_짝수의_합.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120831
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 01. 21. 10:11:01
+# 작성일: 2026. 01. 21. 10:19:08
 
 def solution(n):
-    return sum(i for i in range(2,n+1,2))
+    return sum([i for i in range(2,n+1,2)])
