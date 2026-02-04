7f425407baf699f7cbca83b6038917a52694b751
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-05 00:29:21 +0900

    [프로그래머스] L0 - 머쓱이보다 키 큰 사람 (#19)

--- programmers/L0_기초입문/입문/019_머쓱이보다_키_큰_사람.py
+++ programmers/L0_기초입문/입문/019_머쓱이보다_키_큰_사람.py
@@ -3,8 +3,9 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120585
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 05. 00:27:53
+# 작성일: 2026. 02. 05. 00:29:21
 
 def solution(array, height):
-    array2 = [i for i in array if i > height]
-    return len(array2)
+    array.append(height)
+    array.sort(reverse=True)
+    return array.index(height)
