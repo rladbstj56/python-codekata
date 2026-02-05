ec57aa7d5afb1e1fa96c61fda34fea8ce5af1e60
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-05 09:39:31 +0900

    [프로그래머스] L0 - 배열 자르기 (#21)

--- programmers/L0_기초입문/입문/021_배열_자르기.py
+++ programmers/L0_기초입문/입문/021_배열_자르기.py
@@ -3,7 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120833
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 05. 09:34:51
+# 작성일: 2026. 02. 05. 09:39:31
 
 def solution(numbers, num1, num2):
-    return numbers[num1:num2+1]
+    answer=[]
+    for i in range (num1, num2+1):
+        answer.append(numbers[i])
+    return answer
