19eaed55dedb5552bad99fe53d3c351060a1c81a
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-06 14:35:13 +0900

    [프로그래머스] L0 - 배열의 유사도 (#24)

--- programmers/L0_기초입문/입문/024_배열의_유사도.py
+++ programmers/L0_기초입문/입문/024_배열의_유사도.py
@@ -3,13 +3,12 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120903
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 06. 14:13:39
+# 작성일: 2026. 02. 06. 14:35:13
 
 def solution(s1, s2):
     cnt = 0
-    for s in s2:
-        for a in s1:
-            if s == a:
-                cnt += 1
+    for w in s1:
+        if w in s2:
+            cnt+=1
     return cnt
         
