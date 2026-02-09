8e68bf2addf6102e39164d3d1d17c0220370bab5
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-09 14:21:34 +0900

    [프로그래머스] L0 - n의 배수 고르기 (#26)

--- programmers/L0_기초입문/입문/026_n의_배수_고르기.py
+++ programmers/L0_기초입문/입문/026_n의_배수_고르기.py
@@ -3,11 +3,8 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120905
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 09. 14:18:00
+# 작성일: 2026. 02. 09. 14:21:34
 
 def solution(n, numlist):
-    answer = []
-    for i in numlist:
-        if i % n == 0:
-            answer.append(i)
+    answer = [i for i in numlist if i%n==0]
     return answer
