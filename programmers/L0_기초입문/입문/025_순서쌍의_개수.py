a93cacef67cdc57cd34d022edcf8cb4f764e54ea
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-09 09:21:56 +0900

    [프로그래머스] L0 - 순서쌍의 개수 (#25)

---
+++ programmers/L0_기초입문/입문/025_순서쌍의_개수.py
@@ -0,0 +1,13 @@
+# 순서쌍의 개수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120836
+# 알고리즘: 기초
+# 작성자: 김윤서
+# 작성일: 2026. 02. 09. 09:21:55
+
+def solution(n):
+    cnt = 0
+    for i in range (1,n+1):
+        if n % i == 0:
+            cnt += 1
+    return cnt
