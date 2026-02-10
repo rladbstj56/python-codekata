7a96bda7a9c9f85120aa349235c581ecaf93be05
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-10 09:23:52 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,10 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 김윤서
+# 작성일: 2026. 02. 10. 09:23:51
+
+def solution(money):
+    answer = divmod(money, 5500)
+    return answer
