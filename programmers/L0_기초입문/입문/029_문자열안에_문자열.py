3fc3e50d420c13bc905def7ad2f02f3072b80e3b
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-11 09:11:13 +0900

    [프로그래머스] L0 - 문자열안에 문자열 (#29)

--- programmers/L0_기초입문/입문/029_문자열안에_문자열.py
+++ programmers/L0_기초입문/입문/029_문자열안에_문자열.py
@@ -3,10 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120908
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 11. 09:10:01
+# 작성일: 2026. 02. 11. 09:11:12
 
 def solution(str1, str2):
-    if str2 in str1:
-        return 1
-    else:
-        return 2
+    return 1 if str2 in str1 else 2
