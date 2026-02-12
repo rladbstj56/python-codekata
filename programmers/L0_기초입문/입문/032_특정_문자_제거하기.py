0865f9067a7ef3478b0cdab50edfebb23d800234
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-12 09:57:16 +0900

    [프로그래머스] L0 - 특정 문자 제거하기 (#32)

--- programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
+++ programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
@@ -3,11 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120826
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 12. 09:56:15
+# 작성일: 2026. 02. 12. 09:57:15
 
 def solution(my_string, letter):
-    answer = ''
-    for l in my_string:
-        if l != letter:
-            answer += l
-    return answer
+    return ''.join([l for l in my_string if l != letter])
