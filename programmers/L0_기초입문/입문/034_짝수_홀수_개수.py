806519a455cabe284eaf32e126ce6ba6301840c3
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-13 09:39:14 +0900

    [프로그래머스] L0 - 짝수 홀수 개수 (#34)

--- programmers/L0_기초입문/입문/034_짝수_홀수_개수.py
+++ programmers/L0_기초입문/입문/034_짝수_홀수_개수.py
@@ -3,10 +3,8 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120824
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 13. 09:37:04
+# 작성일: 2026. 02. 13. 09:39:13
 
 def solution(num_list):
-    answer=[0,0]
-    for n in num_list:
-        answer[n%2]+=1
-    return answer
+    odd = [n for n in num_list if n%2]
+    return [len(num_list)-len(odd),len(odd)]
