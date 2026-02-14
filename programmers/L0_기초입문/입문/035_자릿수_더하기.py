2b20e016d9d8fbf4c80d3d93efbcc3d6856e5444
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-15 00:54:14 +0900

    [프로그래머스] L0 - 자릿수 더하기 (#35)

--- programmers/L0_기초입문/입문/035_자릿수_더하기.py
+++ programmers/L0_기초입문/입문/035_자릿수_더하기.py
@@ -3,13 +3,14 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120906
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 15. 00:49:30
+# 작성일: 2026. 02. 15. 00:54:13
 
 def solution(n):
     answer = 0
-    n = str(n)
-    nums = [i for i in n]
-    for i in range(len(nums)):
-        num = nums.pop()
-        answer += int(num)
+    a, b = divmod(n, 10)
+    while a != 0:
+        answer += b
+        n = int(n/10)
+        a, b = divmod(n, 10)
+    answer += b
     return answer
