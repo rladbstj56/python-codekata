2fc466579bb209966cf9abf807b6ad49798c5429
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-15 00:58:53 +0900

    [프로그래머스] L0 - 자릿수 더하기 (#35)

--- programmers/L0_기초입문/입문/035_자릿수_더하기.py
+++ programmers/L0_기초입문/입문/035_자릿수_더하기.py
@@ -3,13 +3,12 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120906
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 15. 00:58:01
+# 작성일: 2026. 02. 15. 00:58:53
 
 def solution(n):
     answer = 0 
     while n:
-        answer += n%10 
-        # n = int(n/10)
-        n //= 10
+        n, r = divmod(n,10)
+        answer += r
     return answer
                
