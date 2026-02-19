ba076b8d187dd228f433ea4dc846c06d6a5513c6
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-20 00:33:40 +0900

    [프로그래머스] 저자 별 카테고리 별 매출액 집계하기 풀이 제출
    - 문제 ID: 144856
    - 난이도: unknown
    - 제출일: 2026. 02. 20. 00:33:39
    - 작성자: 김윤서

--- programmers/other/144856_저자_별_카테고리_별_매출액_집계하기.py
+++ programmers/other/144856_저자_별_카테고리_별_매출액_집계하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144856
 # 작성자: 김윤서
-# 작성일: 2026. 02. 20. 00:29:32
+# 작성일: 2026. 02. 20. 00:33:39
 
 select 
     a.author_id, 
@@ -14,6 +14,6 @@ join author a
 on b.author_id = a.author_id 
 join book_sales bs 
 on b.book_id = bs.book_id
-where bs.sales_date like '2022-01%'
+where bs.sales_date >= '2022-01-01' and bs.sales_date < '2022-02-01'
 group by a.author_id, a.author_name, b.category
 order by a.author_id asc, b.category desc
