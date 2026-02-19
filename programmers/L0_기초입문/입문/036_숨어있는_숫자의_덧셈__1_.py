4bb75ee3a43f01e09b517bd61764991294d3210d
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-19 20:52:26 +0900

    [프로그래머스] L0 - 숨어있는 숫자의 덧셈 (1) (#36)

---
+++ programmers/L0_기초입문/입문/036_숨어있는_숫자의_덧셈__1_.py
@@ -0,0 +1,10 @@
+# 숨어있는 숫자의 덧셈 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120851
+# 알고리즘: 기초
+# 작성자: 김윤서
+# 작성일: 2026. 02. 19. 20:52:25
+
+def solution(my_string):
+    answer = sum([int(i) for i in my_string if i.isdigit()])
+    return answer
