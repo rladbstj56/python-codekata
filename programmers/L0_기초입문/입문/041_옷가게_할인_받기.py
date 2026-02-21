6a585b2b56c5ac6fbf0269619187cc29cd3b61f3
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-02-21 21:06:11 +0900

    [프로그래머스] L0 - 옷가게 할인 받기 (#41)

--- programmers/L0_기초입문/입문/041_옷가게_할인_받기.py
+++ programmers/L0_기초입문/입문/041_옷가게_할인_받기.py
@@ -3,13 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120818
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 02. 21. 21:04:03
+# 작성일: 2026. 02. 21. 21:06:11
 
 def solution(price):
-    if price >= 500000:
-        price *= 0.8
-    elif price >= 300000:
-        price *= 0.9 
-    elif price >= 100000:
-        price *= 0.95
-    return int(price)
+    return int(price*0.8) if price>=500000 else int(price*0.9) if price>=300000 else int(price*0.95) if price>=100000 else int(price)
