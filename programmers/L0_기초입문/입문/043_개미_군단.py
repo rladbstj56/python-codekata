c50238a47270dcf5cbc17331a76d567a746f2bbd
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-03-16 20:30:06 +0900

    [프로그래머스] L0 - 개미 군단 (#43)

--- programmers/L0_기초입문/입문/043_개미_군단.py
+++ programmers/L0_기초입문/입문/043_개미_군단.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120837
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 03. 16. 11:26:25
+# 작성일: 2026. 03. 16. 20:30:05
 
 def solution(hp):
     answer = 0
