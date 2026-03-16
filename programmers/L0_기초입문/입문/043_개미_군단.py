f7924c5566cc51253a0dd7c0c6c9ecbed1a5fbc2
MYUN <80273734+rladbstj56@users.noreply.github.com>
2026-03-16 11:26:25 +0900

    [프로그래머스] L0 - 개미 군단 (#43)

--- programmers/L0_기초입문/입문/043_개미_군단.py
+++ programmers/L0_기초입문/입문/043_개미_군단.py
@@ -3,11 +3,14 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120837
 # 알고리즘: 기초
 # 작성자: 김윤서
-# 작성일: 2026. 03. 16. 11:20:43
+# 작성일: 2026. 03. 16. 11:26:25
 
 def solution(hp):
-    rest = hp
-    jang, rest = divmod(rest,5)
-    byung, rest = divmod(rest,3)
-    il, rest = divmod(rest,1)
-    return jang + byung + il
+    answer = 0
+    ants = [5, 3, 1]
+    
+    for ant in ants:
+        count, hp = divmod(hp, ant)
+        answer += count
+        
+    return answer
